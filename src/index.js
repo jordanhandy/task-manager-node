3bcc4d5732f6f6569238847168ea3245604afef1
Jordan Handy <handy.jordan@gmail.com>
2021-04-20 08:21:23 -0400

    read tasks and task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 // modules
 const express = require("express");
-require("./db/mongoose");
-const User = require("./models/user");
-const Task = require ("./models/task");
-const { Mongoose } = require("mongoose");
-const app = express();
-const PORT = process.env.PORT || 3000;
+require("./db/mongoose"); // require mongoose connection
+const User = require("./models/user"); // User model
+const Task = require ("./models/task"); // Task model
 
-app.use(express.json());
+const app = express(); // set up Express
+const PORT = process.env.PORT || 3000; // set Port
 
-app.post("/users",(req,res)=>{
-    const user = new User(req.body);
+app.use(express.json()); // Receive JSON back from Express
+
+app.post("/users",(req,res)=>{ // POST to users
+    const user = new User(req.body); // use the request as the JSON body
     user.save().then(() =>{
         res.status(201).send(user);
     }).catch((e)=>{
@@ -18,8 +18,8 @@ app.post("/users",(req,res)=>{
     })
 
 })
-app.post("/tasks",(req,res) =>{
-    const task = new Task(req.body);
+app.post("/tasks",(req,res) =>{ // POST to tasks
+    const task = new Task(req.body); // use the request as the JSON body
     task.save().then((task) => {
         res.status(201).send(task);
     }).catch((error)=>{
@@ -28,7 +28,7 @@ app.post("/tasks",(req,res) =>{
 })
 
 app.get("/users",(req,res)=>{
-    User.find({}).then((users) =>{
+    User.find({}).then((users) =>{ // find all (no criteria)
         res.status(200).send(users);
     }).catch((error)=>{
         res.status(500).send(error);
@@ -36,12 +36,29 @@ app.get("/users",(req,res)=>{
 })
 
 app.get("/users/:id",(req,res)=>{
-    const _id = req.params.id;
+    const _id = req.params.id; // find by ID
     User.findById(_id).then((user)=>{
         if(!user){
             return res.status(404).send();
         }
         res.status(200).send(user);
+    }).catch((error)=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((tasks)=>{ // find all (no params)
+        res.status(200).send(tasks)
+    }).catch((error)=>{
+        res.status(500).send(error)
+    });
+})
+
+app.get('/tasks/:id',(req,res)=>{
+    const _id = req.params.id; // find by ID
+    Task.findById(_id).then((task)=>{
+        res.status(200).send(task)
     }).catch((error)=>{
         res.status(500).send(error);
     })
