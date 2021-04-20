e44f14e75434a73040ac3a1f79c4f3b6ff2ae799
Jordan Handy <handy.jordan@gmail.com>
2021-04-19 23:08:06 -0400

    refactor models to own directory, set up post requests

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,49 +1,10 @@
 const mongoose = require('mongoose');
-const validator = require("validator");
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology:true
 });
-const User = mongoose.model('User',{
-    name:{
-        type: String,
-        required: true,
-        trim: true
-    },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Email is invalid");
-            }
-        }
 
-    },
-    password:{
-        type:String,
-        required:true,
-        minlength:7,
-        trim:true,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error("Password cannot contain password");
-            }
-        }
-    },
-    age:{
-        type: Number,
-        default:0,
-        validate(value){
-            if(value < 0){
-                throw new Error("Age must be a positive number!");
-            }
-        }
-    }
-})
 // const me = new User({
 //     name:"Jordan",
 //     age:28,
@@ -57,23 +18,12 @@ const User = mongoose.model('User',{
 //     console.log("Failure",error);
 
 // });
-const Task = mongoose.model('Task',{
-    description:{
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed:{
-        type: Boolean,
-        required: false,
-        default: false
-    }
-});
-const task = new Task({
-    description:"Clean room    "
-});
-task.save().then((result) =>{
-    console.log(result,"Saved to database");
-}).catch((error)=>{
-    console.log("Error",error);
-})
+
+// const task = new Task({
+//     description:"Clean room    "
+// });
+// task.save().then((result) =>{
+//     console.log(result,"Saved to database");
+// }).catch((error)=>{
+//     console.log("Error",error);
+// })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 // modules
 const express = require("express");
+const User = require("./models/user");
+require("./db/mongoose");
+require("./models/user");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.post("/users",(req,res)=>{
+    const user = new User(req.body);
+    user.save().then((result) =>{
+        res.send("User created successfully",user);
+    }).catch((error)=>{
+        res.send("Unable to create user!",error);
+    })
+
+})
+
 app.listen(PORT,() =>{
-    console.log("Server is listening on port" + PORT);
+    console.log("Server is listening on port " + PORT);
 });
