ec425f3972fa0d33efd505de174eda62ef9c003e
Jordan Handy <handy.jordan@gmail.com>
2021-04-19 22:51:32 -0400

    add express

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// modules
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,() =>{
+    console.log("Server is listening on port" + PORT);
+});
