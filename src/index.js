1a646312a44dfca99ac9861e17cc301c6b69714a
Jordan Handy <handy.jordan@gmail.com>
2021-04-20 21:52:04 -0400

    delete task

--- src/index.js
+++ src/index.js
@@ -70,13 +70,13 @@ app.patch('/users/:id',async(req,res) =>{
 
 app.delete("/users/:id",async(req,res) => {
     try{
-        const user = await User.findByIdAndDelete(req.params.id);
+        const user = await User.findByIdAndDelete(req.params.id); // find user by id
         if(!user){
-            return res.status(404).send();
+            return res.status(404).send(); // if user not found
         }
         res.status(200).send(user);
     }catch(e){
-        res.status(500).send(e);
+        res.status(500).send(e); // if error
     }
 })
 
@@ -130,6 +130,18 @@ app.patch('/tasks/:id',async(req,res)=>{
         res.status(200).send(task);
     }catch(e){
         res.status(400).send(e)
+    }
+})
+
+app.delete("/tasks/:id",async(req,res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id); // find by id
+        if(!task){
+            return res.status(404).send();
+        }
+        res.status(200).send(task);
+    }catch(e){
+        res.status(500).send(e);
     }
 })
 
